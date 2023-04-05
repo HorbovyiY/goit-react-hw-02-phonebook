c399d04144f937b4e5315d5b9fb21d8a1915804a
HorbovyiY <yhorbovyi@gmail.com>
2023-04-05 09:29:53 +0300

    додає addContact

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,40 @@
-export const App = () => {
-  return (
+import React from "react";
+import { nanoid } from "nanoid";
+
+export class App extends React.Component {
+  state = {
+  contacts: [],
+  name: ''
+  }
+
+  handleNameChange = (e) => { 
+    this.setState({name: e.currentTarget.value})
+  }
+
+  resetForm = () => {
+    this.setState({name: ''})
+  }
+
+  addContact = (e) => { 
+    const id = nanoid();
+    const name = this.state.name;
+    const contact = {
+      id,
+      name
+    }
+
+    e.preventDefault();
+
+    this.setState((prevState) => ({ 
+      contacts: [contact, ...prevState.contacts]
+    })
+    )
+
+    this.resetForm();
+  }
+
+  render() { 
+    return(
     <div
       style={{
         height: '100vh',
@@ -10,7 +45,28 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <h2>Phonebook</h2>  
+      <form onSubmit={this.addContact}>
+        <label>
+            Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            value={this.state.name}
+            onChange={this.handleNameChange}
+            required
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </form>
+        
+
+        <h2>Contacts</h2>  
+        <span></span>
     </div>
   );
+  }
+  
 };
